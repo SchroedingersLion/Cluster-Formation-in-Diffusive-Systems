166ca92680024ba9d05fa99811530ca2b370f2ff
Rene Lohmann <r.lohmann@ed.ac.uk>
2025-01-06 22:39:33 +0100

    removed coordinate vector. all vectors (positions, forces etc.) are now of shape (Nparticles, dimension)

--- src/model.h
+++ src/model.h
@@ -9,10 +9,6 @@
 #include <iomanip>
 
 
-struct coordinate{    // Used to denote positions, velocities and forces.
-    double x{0};
-};
-
 
 // ###################### Model CLASS DEFINITION ##################################################################################
 
@@ -23,31 +19,30 @@ class IPS_model {
         // MEMBERS THAT WILL BE ACCESSED BY MEASUREMENT OR SIMULATION CLASSES
         const double L;                // Box volume = [-L,L]^2.
         const int N_particles;         // No. of particles.
-        std:: vector <coordinate> positions, init_positions, velocities, forces;
-        coordinate (IPS_model::* get_force_ij) (const coordinate); // Points to interaction function between two particles.
-        coordinate get_distances_ij(const size_t i, const size_t j);                 // Get (dx, dy) tupel of distances between particles i,j.
+        const size_t dimension;
+        std:: vector <std:: vector <double>> positions, init_positions, velocities, forces;
+        std:: vector <double> (IPS_model::* get_force_ij) (const std:: vector <double>); // Points to interaction function between two particles.
+        std:: vector <double> get_distances_ij(const size_t i, const size_t j);                 // Get (dx, dy) tupel of distances between particles i,j.
 
         // CONSTRUCTOR.
-        IPS_model(const int N_particles, const double boxlength, const std:: string& forcefield)
+        IPS_model(const int N_particles, const double boxlength, const std:: string& forcefield, const size_t dimension)
                 : N_particles {N_particles}, 
                   L {boxlength/2}, 
                   kappa {1./N_particles}, 
-                  forcefield {forcefield} 
+                  forcefield {forcefield},
+                  dimension {dimension}, 
             {
 
                 std:: cout  << "Building IPS model with boxlength " << boxlength << " and " << N_particles << " particles.\n"
                             << "Forcefield: " << forcefield << std:: endl;
 
                 // Resize vectors.
-                positions.resize(N_particles);
-                init_positions.resize(N_particles);
-                velocities.resize(N_particles);
-                forces.resize(N_particles);
+                resize_vectors();
 
                 // Specify force field.
                 if (forcefield=="gauss") get_force_ij = &IPS_model:: get_force_ij_gauss;
-                else if (forcefield=="morse") get_force_ij = &IPS_model:: get_force_ij_morse;
-                else if (forcefield=="gem4") get_force_ij = &IPS_model:: get_force_ij_gem4;
+                // else if (forcefield=="morse") get_force_ij = &IPS_model:: get_force_ij_morse;
+                // else if (forcefield=="gem4") get_force_ij = &IPS_model:: get_force_ij_gem4;
                 else throw std:: invalid_argument( "Invalid forcefield in model construction. Allowed are 'gauss', 'morse', and 'gem4'." );
 
             }
@@ -56,17 +51,19 @@ class IPS_model {
 
         const double kappa;             // Interaction potential prefactor (see paper).
         const std:: string forcefield;  // Specifies interaction potential.
+        void resize_vectors();
         
         // ########## FORCES ###############################################################################################################        
         // Gaussian potential.
-        coordinate get_force_ij_gauss(const coordinate distance);            // Gauss interaction function.
+        std:: vector <double> get_force_ij_gauss(const std:: vector <double> distance);            // Gauss interaction function.
 
         // Morse potential.
-        coordinate get_force_ij_morse(const coordinate distance);            // Morse interaction function.
+        // coordinate get_force_ij_morse(const coordinate distance);            // Morse interaction function.
 
-        // GEM-4 potential
-        coordinate get_force_ij_gem4(const coordinate distance);            // GEM-4 interaction function.
-        double my_pow(double x, int n);  // Help function used in get_force_ij_gauss.
+        // // GEM-4 potential
+        // coordinate get_force_ij_gem4(const coordinate distance);            // GEM-4 interaction function.
+        
+        double my_pow(double x, int n); // Help function to compute x^n.
 
         //##################################################################################################################################
 
@@ -75,15 +72,27 @@ class IPS_model {
 
 
 // ##################### INLINE MEMBER FUNCTION DEFINITIONS ###################################
-inline coordinate IPS_model:: get_distances_ij(const size_t i, const size_t j){
+inline void IPS_model:: resize_vectors(){
+
+    positions.resize(N_particles, std:: vector<double>(dimension));
+    init_positions.resize(N_particles, std:: vector<double>(dimension));
+    velocities.resize(N_particles, std:: vector<double>(dimension));
+    forces.resize(N_particles, std:: vector<double>(dimension));
+
+}
+
+
+inline std:: vector <double> IPS_model:: get_distances_ij(const size_t i, const size_t j){
 
     const double two_L {2*L};
+    double dx;
+    std:: vector <double> distances(dimension);
 
-    double dx {positions[i].x - positions[j].x};
-
-    dx = dx > L ? dx - two_L : (dx < -L ? dx + two_L : dx);
-
-    coordinate distances {dx};
+    for (size_t dim = 0; dim<dimension; ++dim){
+        dx = positions[i][dim] - positions[j][dim];
+        dx = dx > L ? dx - two_L : (dx < -L ? dx + two_L : dx);
+        distances[dim] = dx;
+    }
 
     return distances;
 
@@ -92,15 +101,17 @@ inline coordinate IPS_model:: get_distances_ij(const size_t i, const size_t j){
 
 // Gaussian potential.
 const double sigma_2_gauss {0.5}; // sigma^2.
-inline coordinate IPS_model:: get_force_ij_gauss(const coordinate distance){
+inline std:: vector <double> IPS_model:: get_force_ij_gauss(const std:: vector <double> distance){
 
     
-    const double dist_sq {distance.x*distance.x};
+    double dist_sq {0};
+    for (size_t dim = 0; dim<dimension; ++dim) dist_sq += distance[dim]*distance[dim];
 
     const double pref {-kappa/(sigma_2_gauss)};
     const double expo_term {pref * exp(-dist_sq/(2*sigma_2_gauss))};
 
-    coordinate force_ij {expo_term*distance.x};
+    std:: vector <double> force_ij(dimension);
+    for (size_t dim = 0; dim<dimension; ++dim) force_ij[dim] = expo_term*distance[dim];
 
     return force_ij;
 
@@ -131,54 +142,54 @@ inline coordinate IPS_model:: get_force_ij_gauss(const coordinate distance){
 
 
 // GEM-Alpha potential (reduces to Gaussian for alpha=2, but is less efficient compute-wise).
-const double sigma_2_gem4 {0.5};
-const double sqrt_two_sigma_2_gem4 {sqrt(2*sigma_2_gem4)};
-const int alpha_gem4 {4};
-const double pref_gem4 {alpha_gem4 / pow(sqrt_two_sigma_2_gem4, alpha_gem4)};
-inline coordinate IPS_model:: get_force_ij_gem4(const coordinate distance){
+// const double sigma_2_gem4 {0.5};
+// const double sqrt_two_sigma_2_gem4 {sqrt(2*sigma_2_gem4)};
+// const int alpha_gem4 {4};
+// const double pref_gem4 {alpha_gem4 / sqrt_two_sigma_2_gem4};
+// inline coordinate IPS_model:: get_force_ij_gem4(const coordinate distance){
 
-    const double normed_distance {sqrt(distance.x*distance.x) / sqrt_two_sigma_2_gem4};
-    const double exponent {my_pow(normed_distance, alpha_gem4)};
+//     const double normed_distance {sqrt(distance.x*distance.x) / sqrt_two_sigma_2_gem4};
+//     const double exponent {my_pow(normed_distance, alpha_gem4)};
 
-    const double pref {-kappa*pref_gem4*exponent/normed_distance};
-    const double expo_term {pref * exp(-exponent)};
+//     const double pref {-kappa*pref_gem4*exponent/normed_distance};
+//     const double expo_term {pref * exp(-exponent)};
 
-    coordinate force_ij {distance.x > 0 ? expo_term : -expo_term};
+//     coordinate force_ij {distance.x > 0 ? expo_term : -expo_term};
 
-    return force_ij;
+//     return force_ij;
 
-}
-
-
-inline double IPS_model:: my_pow(double x, int n){
-    double r = 1.0;
-
-    while(n > 0){
-        r *= x;
-        --n;
-    }
-
-    return r;
-}
+// }
 
 
-// Morse potential.
-const double a_morse {2};
-const double r_morse {0}; 
-const double D_morse {1};
+// inline double IPS_model:: my_pow(double x, int n){
+//     double r = 1.0;
 
-inline coordinate IPS_model:: get_force_ij_morse(const coordinate distance){
+//     while(n > 0){
+//         r *= x;
+//         --n;
+//     }
+
+//     return r;
+// }
+
+
+// // Morse potential.
+// const double a_morse {2};
+// const double r_morse {0}; 
+// const double D_morse {1};
+
+// inline coordinate IPS_model:: get_force_ij_morse(const coordinate distance){
     
-    const double dist {sqrt(distance.x*distance.x)};
+//     const double dist {sqrt(distance.x*distance.x)};
 
-    const double expo {exp(-a_morse * (dist-r_morse))}; 
-    const double pref {kappa * 2* a_morse * D_morse * (expo*expo-expo) / dist};
+//     const double expo {exp(-a_morse * (dist-r_morse))}; 
+//     const double pref {kappa * 2* a_morse * D_morse * (expo*expo-expo) / dist};
 
-    coordinate force_ij {pref*distance.x};
+//     coordinate force_ij {pref*distance.x};
 
-    return force_ij;
+//     return force_ij;
 
-}
+// }
 
 // ########### END OF MEMBER DEFINITIONS ##############################################
 
