9b88ec7a940b3e239b039ff7f865fa8ba35698bd
Rene Lohmann <r.lohmann@ed.ac.uk>
2025-01-25 01:52:08 +0100

    fixed constructor so that dims > 3 are not possible

--- src/coordinate.h
+++ src/coordinate.h
@@ -18,7 +18,8 @@ class coordinate {
         }
 
         coordinate(size_t dim)
-            : dimension {dim} {
+        {
+                set_dimension(dim);
                 coords.fill(0.0);
         }
 
@@ -32,16 +33,16 @@ class coordinate {
 
         // Access operator for reading/writing within the runtime dimension.
         double& operator[](size_t index) {
-            // if (index >= dimension) {
-            //     throw std::out_of_range("Index exceeds runtime dimension.");
-            // }
+            if (index >= dimension) {
+                throw std::out_of_range("Index exceeds runtime dimension.");
+            }
             return coords[index];
         }
 
         const double& operator[](size_t index) const {
-            // if (index >= dimension) {
-            //     throw std::out_of_range("Index exceeds runtime dimension.");
-            // }
+            if (index >= dimension) {
+                throw std::out_of_range("Index exceeds runtime dimension.");
+            }
             return coords[index];
         }
 
