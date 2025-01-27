ceb7fbf52aad232c56482dc94ecfc9685eb0feb3
Rene Lohmann <r.lohmann@ed.ac.uk>
2025-01-27 18:03:45 +0100

    turned coordinate into template

--- src/coordinate.h
+++ src/coordinate.h
@@ -5,53 +5,28 @@
 #include <iostream>
 #include <stdexcept>
 
-constexpr size_t MAXDIM = 1; // Maximum allowed dimension.
-
+template <size_t DIMENSION>
 class coordinate {
 
     public:
         
         // Default constructor.
         coordinate() 
-            : dimension {0} {
-                coords.fill(0.0); // Initialize all elements to 0.0.
-        }
-
-        coordinate(size_t dim)
         {
-                set_dimension(dim);
-                coords.fill(0.0);
-        }
-
-        // Method to set the runtime dimension.
-        void set_dimension(size_t dim) {
-            if (dim > MAXDIM) {
-                throw std::out_of_range("Dimension exceeds MAXDIM.");
-            }
-            dimension = dim;
+                coords.fill(0.0); // Initialize all elements to 0.0.
         }
 
         // Access operator for reading/writing within the runtime dimension.
         double& operator[](size_t index) {
-            // if (index >= dimension) {
-            //     throw std::out_of_range("Index exceeds runtime dimension.");
-            // }
             return coords[index];
         }
 
         const double& operator[](size_t index) const {
-            // if (index >= dimension) {
-            //     throw std::out_of_range("Index exceeds runtime dimension.");
-            // }
             return coords[index];
         }
 
     private:
-        std::array<double, MAXDIM> coords; // Fixed-size array.
-        size_t dimension;                  // Actual dimension.
-
-        // Getters
-        size_t size() const { return dimension; }
+        std::array<double, DIMENSION> coords; // Fixed-size array.
 
 };
 
